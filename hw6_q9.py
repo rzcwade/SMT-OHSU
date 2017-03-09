4c7c0dd4348bff8f4e784b5b8b63a0a7b6be5f06
rzcwade <renzicheng@yahoo.com>
2017-03-08 23:57:28 -0800

    some changes

--- hw6_q9.py
+++ hw6_q9.py
@@ -2,14 +2,14 @@ from hw4standard import *
 
 # use freshvariables, unify, substitute
 
-kb = [
+cnf = [
         ['ostrich','sam'],
         ['canary','tweety'],
-        [ ['bird','X'],['ostrich','X'] ],
-        [ ['bird','X'],['canary','X'] ],
-        [ ['fly','X'],['bird','X'],['normal','X'] ],
-        [ ['not',['normal','X']],['ostrich','X'] ],
-        [ ['normal','X'],['canary','X'] ],
+        [ ['bird','X'],['not',['ostrich','X']] ],
+        [ ['bird','X'],['not',['canary','X']] ],
+        [ ['fly','X'],['not',['bird','X']],['not',['normal','X']] ],
+        [ ['not',['normal','X']],['not',['ostrich','X']] ],
+        [ ['normal','X'],['not',['canary','X']] ],
      ]
 
 def sameclause(a,b):
@@ -49,16 +49,29 @@ def prettyCNF(cnf):
 def prove(query, kb):
         #take a query(a positive and negative literal) and a KB,
         #determine if KB |= query
-        
+        neq_query = negate(query)
         #Let the consequent set be the initial KB
-        
         #add negation to the KB and see if it's derived false
-        
+        kb.append(neq_query)
         #foreach zip{clauseA : clauseB}:
         #       if clause_ is unit clause & resolved:
         #               if result not in consequence set:
         #                       add result
         #                       check if no more added and consequence set is {}
         #return True if proved and print
+        while kb != []:
+            for rule in cnf:
+                rule = freshvariables(rule)
+                if ((rule[0] == 'not') and (kb[0][0] != 'not')) or \
+                   ((rule[0] != 'not') and (kb[0][0] == 'not')):
+                       if sameclause(rule[0],kb[0]) and (rule[1:] not in kb):
+                           print("can be resolved")
+                           kb = kb[1:]+rule[1:]
+                
+        
+            return True
+        
+        
         return False
         
+prove(['fly','sam'],[])
